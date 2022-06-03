7336b72b96131f23f5dc3cb8de5dee8c7f74853f
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-04 00:20:05 +0100

    Add a new file for all constants through out the app

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+///App Colors
+Color purpleColor = const Color(0xFF5D4176);
+
+///App TextStyles
+const headerTextStyle = TextStyle(color: Colors.white,
+    fontSize: 24, fontWeight: FontWeight.w700);
+final subHeaderTextStyle = TextStyle(color: Colors.white.withOpacity(0.6),
+    fontSize: 14, fontWeight: FontWeight.w400);
