78ea618cd3544ff44dcd26aad09acca6e30cac4c
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-06 00:13:32 +0100

    Create a downloads view

---
+++ lib/views/downloads_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DownloadsView extends StatefulWidget {
+  const DownloadsView({Key? key}) : super(key: key);
+
+  @override
+  State<DownloadsView> createState() => _DownloadsViewState();
+}
+
+class _DownloadsViewState extends State<DownloadsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
