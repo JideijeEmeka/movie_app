2629bc0c30705ddb8702b4691bb6526bb99e374e
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-06 00:13:43 +0100

    Create a coming soon view

---
+++ lib/views/coming_soon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComingSoon extends StatefulWidget {
+  const ComingSoon({Key? key}) : super(key: key);
+
+  @override
+  State<ComingSoon> createState() => _ComingSoonState();
+}
+
+class _ComingSoonState extends State<ComingSoon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
