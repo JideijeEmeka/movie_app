49b178f4b595312020962d23fa5f437a6c5ea84e
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-05 14:50:14 +0100

    Add a Series view

---
+++ lib/views/series_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SeriesView extends StatefulWidget {
+  const SeriesView({Key? key}) : super(key: key);
+
+  @override
+  State<SeriesView> createState() => _SeriesViewState();
+}
+
+class _SeriesViewState extends State<SeriesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
