48dba9a84e554dabb61d5b2d5a8430ad3f3d3a47
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-09 01:06:36 +0100

    Fetch necessary details of a movie from api

--- lib/views/play_movie_view.dart
+++ lib/views/play_movie_view.dart
@@ -1,10 +1,13 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app/helpers/constants.dart';
 
 class PlayMovieView extends StatefulWidget {
-  const PlayMovieView({Key? key}) : super(key: key);
+  final String name, description, bannerUrl, posterUrl, vote, launchOn;
+  const PlayMovieView({Key? key, required this.name, required this.description,
+    required this.bannerUrl, required this.posterUrl, required this.vote,
+    required this.launchOn}) : super(key: key);
 
   @override
   State<PlayMovieView> createState() => _PlayMovieViewState();
@@ -15,79 +18,101 @@ class _PlayMovieViewState extends State<PlayMovieView> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/images/movie_img2.png"),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(image: NetworkImage("https://image.tmdb.org/t/p/w500"
+                  + widget.bannerUrl),
               fit: BoxFit.fill)
             ),
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                  const SizedBox(height: 50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        onTap: () => Navigator.pop(context),
-                        child: const ImageIcon(AssetImage("assets/images/back_button.png"),
-                            color: Colors.white),
-                      ),
-                      Text("Overview", style: appBarTextStyle),
-                      Container(width: 50)]),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    child: Center(child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                        child: Image.asset("assets/images/movie_img2.png", fit: BoxFit.cover,))),
-                  ),
-                  Text("Black Widow", style: appBarTextStyle,),
-                  const SizedBox(height: 5,),
-                  IntrinsicHeight(
+              child: Column(
+                children: [
+                  Container(height: 30, color: Colors.black.withOpacity(0.3),),
+                  Container(
+                    height: 60,
+                    color: Colors.black.withOpacity(0.3),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text("Eng", style: subHeaderTextStyle,),
-                        VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
-                        Text("Action, Sci-Fi", style: subHeaderTextStyle,),
-                        VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
-                        Text("3hrs 10min", style: subHeaderTextStyle,),
-                        VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
-                        Text("PG", style: subHeaderTextStyle,),
-                        VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
-                        Image.asset("assets/images/star_rating.png"),
-                        Text("7.4 IMDb", style: subHeaderTextStyle,),
-                        VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
-                      ],
-                    ),
-                  ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30, bottom: 10),
-                      child: Text("Story line", style: titleTextStyle,),
-                    ),
-                    Text("Natasha Romanoff, aka Black Widow, confronts the "
-                      "darker parts of her ledger when a dangerous "
-                      "conspiracy with ties to her past arises. Pursued by a "
-                      "force that will read more...", style: subHeaderTextStyle,),
-                    Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.symmetric(vertical: 30),
-                      child: ElevatedButton(onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            textStyle: titleTextStyle,
-                            primary: gradientRedColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          InkWell(
+                            onTap: () => Navigator.pop(context),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 20),
+                              child: ImageIcon(AssetImage("assets/images/back_button.png"),
+                                  color: Colors.white),
                             ),
-                            padding: const EdgeInsets.symmetric(vertical: 10)),
-                          child: const Text("Watch")),
-                    )
-                ],),
+                          ),
+                          Text("Overview", style: appBarTextStyle),
+                          Container(width: 50)]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        child: Center(child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                            child: Image.network("https://image.tmdb.org/t/p/w500"
+                                + widget.posterUrl, fit: BoxFit.cover,))),
+                      ),
+                    ],),
+                  ),
+                  Container(
+                    color: Colors.black.withOpacity(0.3),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Text(widget.name, style: appBarTextStyle,),
+                      ),
+                      const SizedBox(height: 5,),
+                      IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("Eng", style: subHeaderTextStyle,),
+                            VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
+                            Text("Action, Sci-Fi", style: subHeaderTextStyle,),
+                            VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
+                            Text(widget.launchOn, style: subHeaderTextStyle,),
+                            VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
+                            Text("PG", style: subHeaderTextStyle,),
+                            VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
+                            Image.asset("assets/images/star_rating.png", color: Colors.white,),
+                            Text(widget.vote, style: subHeaderTextStyle,),
+                            VerticalDivider(color: Colors.white.withOpacity(0.6), thickness: 2,),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, bottom: 10),
+                        child: Text("Story line", style: appBarTextStyle,),
+                      ),
+                      Text(widget.description, style: descTextStyle),
+                      Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.symmetric(vertical: 30),
+                        child: ElevatedButton(onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                textStyle: titleTextStyle,
+                                primary: gradientRedColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)
+                                ),
+                                padding: const EdgeInsets.symmetric(vertical: 10)),
+                            child: const Text("Watch")),
+                      )
+                    ],),
+                  ),
+                ],
               ),
             ),
           ),
