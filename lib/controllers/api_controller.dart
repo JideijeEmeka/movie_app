9796075680d317ff55ff8fada00888cad14e2504
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-18 22:56:26 +0100

    Remove functions that doesn't work from the controller

--- lib/controllers/api_controller.dart
+++ lib/controllers/api_controller.dart
@@ -16,7 +16,6 @@ class ApiServiceController extends ControllerMVC {
   ConnectivityResult result = ConnectivityResult.none;
   List<String> Flist = [];
   List<String> myList = [];
-  List detailsList = [];
 
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   late SharedPreferences prefs;
@@ -27,11 +26,8 @@ class ApiServiceController extends ControllerMVC {
 
     if(prefs.getStringList('savedList') == null) {
       prefs.setStringList('savedList', []);
-      //return [];
-      //(prefs.containsKey('savedList') ? prefs.getStringList('savedList') : 0)!;
     }
     List<String> list = prefs.getStringList('savedList')!;
-
     setState(() {
       Flist = list;
     });
@@ -52,27 +48,6 @@ class ApiServiceController extends ControllerMVC {
       result = error.toString();
     }
     return result;
-  }
-
-  Future addMovieToList(String id) async {
-    Map detailsMovieResults = await tmdbWithCustomLogs.v3.movies.getDetails(int.parse(id));
-    setState(() {
-      detailsList = detailsMovieResults['results'];
-    });
-  }
-
-  
-  Future addMovie(BuildContext context, String movieId) async {
-    if(tappedOnce == 2 && tapped == true) {
-      /// Populate my local List with fetched list on first click
-      myList.add(movieId);
-      prefs.setInt('savedList', int.parse(myList.toString()));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar(message: "Added to Favorite!"));
-    }else if(tappedOnce == 3){
-      /// Remove movie from my local List on second click
-      ScaffoldMessenger.of(context).showSnackBar(snackBar(message: "Removed from Favorite!"));
-    }
-    prefs.setStringList('savedList', myList);
   }
 
   checkInternetConnection() async {
@@ -108,23 +83,6 @@ class ApiServiceController extends ControllerMVC {
   List similarMovies = [];
   List tvAiringToday = [];
   List popularTvShows = [];
-  List myFavList = [];
-  // List myFavoriteMoviesList = [];
-  // List myFavoriteMovies = [];
-
-  createList() async {
-    Map myFavMoviesList = await tmdbWithCustomLogs.v3.lists.createList('20', 'lol', 'okay');
-    setState(() {
-      myFavList = myFavMoviesList[0]['list_id'];
-    });
-  }
-
-  addMovieItem() async {
-    Map added = await tmdbWithCustomLogs.v3.lists.addItem('20', 'list_id', 2);
-    setState(() {
-      myFavList.insert(0, popularMovies);
-    });
-  }
 
   TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, readAccessToken),
       logConfig: const ConfigLogger(
@@ -158,43 +116,6 @@ class ApiServiceController extends ControllerMVC {
       popularTvShows = popularTvShowsResults['results'];
     });
   }
-
-
-  // addPopularMoviesToList() async {
-  //   // print('$popularMovies');
-  //   final prefs = await SharedPreferences.getInstance();
-  //   prefs.setStringList('savedList', myList as List<String>);
-  //     setState(() {
-  //       if(myList.isEmpty) {
-  //         prefs.getStringList('savedList');
-  //       }
-  //       myList = popularMovies as List<String>;
-  //     });
-  //     print('$myList');
-  // }
-  //
-  // addMovieToList(BuildContext context) async {
-  //   print('$myFavoriteMovies');
-  //   final prefs = await SharedPreferences.getInstance();
-  //   var listId = prefs.getInt('listId');
-  //
-  //     Map myFavMoviesList = await tmdbWithCustomLogs.v3.lists.createList('20', 'lol', 'okay');
-  //     Map myList = await tmdbWithCustomLogs.v3.lists.addItem('20', 'listId', 2);
-  //
-  //     listId = myFavMoviesList["list_id"];
-  //
-  //     // store listId in e.g sharedPrefs
-  //     await prefs.setInt('listId', listId!);
-  //
-  //   // Map myFavMoviesResults = await tmdbWithCustomLogs.v3.lists.addItem('emmy', listId.toString(), 2);
-  //   setState(() {
-  //     myFavoriteMovies = myList['results'];
-  //   });
-  //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
-  //   Text('You added a movie', style: titleTextStyle,), elevation: 5,
-  //     backgroundColor: Colors.brown.withOpacity(0.4),
-  //     margin: const EdgeInsets.only(bottom: 100), behavior: SnackBarBehavior.floating,));
-  // }
 
   ApiServiceController();
 
