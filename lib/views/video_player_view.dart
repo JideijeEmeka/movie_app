762dbbdd2d69f8e106de09ae884413cd1610f11f
Emeka Jideije <jideije.emeka@gmail.com>
2022-06-29 02:25:23 +0100

    Implement the video player view

--- lib/views/video_player_view.dart
+++ lib/views/video_player_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPlayerView extends StatefulWidget {
   const VideoPlayerView({Key? key}) : super(key: key);
@@ -9,28 +10,45 @@ class VideoPlayerView extends StatefulWidget {
 }
 
 class _VideoPlayerViewState extends State<VideoPlayerView> {
+  late VideoPlayerController _playerController;
 
   @override
   void initState() {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
     super.initState();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+    _playerController = VideoPlayerController.network
+      ('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
+      ..initialize().then((_) => {
+        setState(() {
+
+        })
+      });
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _playerController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: BackButton(onPressed: () => {
-        Navigator.pop(context),
-        SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-        },
-        color: Colors.black,),
-      ),
-      body: Container(
-        child: Center(child: Text('Breast'))
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _playerController.value.isPlaying
+                ? _playerController.pause()
+                : _playerController.play();
+          });
+        }, child: Icon(_playerController.value.isPlaying
+            ? Icons.pause : Icons.play_arrow)),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: _playerController.value.isInitialized
+            ? AspectRatio(aspectRatio: _playerController.value.aspectRatio,
+          child: VideoPlayer(_playerController),)
+            : Container())
     );
   }
 }
