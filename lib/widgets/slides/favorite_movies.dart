597a2bba0f6be826c25b335f85d8473e7d112730
Emeka Jideije <jideije.emeka@gmail.com>
2022-07-02 08:17:22 +0100

    Append the list widget to favorite movies list

--- lib/widgets/slides/favorite_movies.dart
+++ lib/widgets/slides/favorite_movies.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/controllers/api_controller.dart';
 import 'package:movie_app/helpers/constants.dart';
-import 'package:movie_app/views/play_movie_view.dart';
+import 'package:movie_app/widgets/my_list_widget.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class FavoriteMovieList extends StatefulWidget {
   final List favoriteList;
@@ -46,28 +45,7 @@ class _FavoriteMovieListState extends StateMVC<FavoriteMovieList> {
           physics: const ScrollPhysics(),
           itemCount: myOwnList.length,
           itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                print(myOwnList);
-                pushNewScreen(context, screen: PlayMovieView(
-                  name: myOwnList[index],
-                  bannerUrl: myOwnList[index],
-                  posterUrl: myOwnList[index],
-                  description: myOwnList[index],
-                  vote: myOwnList[index],
-                  launchOn: myOwnList[index],
-                  language: myOwnList[index],
-                  popularity: myOwnList[index],
-                movieId: myOwnList[index]),
-                    withNavBar: false);
-              },
-              child: Container(
-                  width: 117, height: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(image: NetworkImage(myOwnList[index]))),
-                  margin: const EdgeInsets.only(left: 7)),
-            );
+            return MyListWidget(movieId: myOwnList[index]);
           }),
     );
   }
