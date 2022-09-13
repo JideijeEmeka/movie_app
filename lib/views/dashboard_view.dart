5a511bf8dd266b0c736766b37f0e78fd2f3347d5
Emeka Jideije <jideije.emeka@gmail.com>
2022-09-13 22:39:33 +0100

    update

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -14,7 +14,7 @@ class _DashboardViewState extends State<DashboardView> {
     return const WillPopScope(  
       onWillPop: null,
       child: Scaffold(
-        body: NavBar(), 
+        body: NavBar(),  
       ));
   }
 }
