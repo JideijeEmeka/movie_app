a5a67a8f0160ad0f5072d2d7211ddd8276c7000a
Emeka Jideije <jideije.emeka@gmail.com>
2023-01-04 19:31:02 +0100

    Update text

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return OverlaySupport.global(
       child: MaterialApp(  
-        title: 'EmmyFlix',
+        title: 'EmmyFlix Net',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
