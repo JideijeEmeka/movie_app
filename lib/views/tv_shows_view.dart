8a69de428b506c0c961455edf19a1cdfe3599991
Emeka Jideije <jideije.emeka@gmail.com>
2023-08-25 15:24:10 +0100

    edit

--- lib/views/tv_shows_view.dart
+++ lib/views/tv_shows_view.dart
@@ -35,7 +35,7 @@ class _TvShowsViewState extends StateMVC<TvShowsView> {
     return Scaffold(
       appBar: AppBar(
           automaticallyImplyLeading: false,
-          title: const Text('Tv Shows'),
+          title: const Text('Tv Show'),
           titleTextStyle: loadingTextStyle,
           elevation: 0.0,
           centerTitle: true,
